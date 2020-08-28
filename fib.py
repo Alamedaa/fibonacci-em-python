92643dc8a9c52a9cd18631da6716964239cbf589
Alameda <43188449+Alamedaa@users.noreply.github.com>
2020-08-28 02:14:41 -0300

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,6 +1,20 @@
-num = 100
-a , b = 0 , 1
-while a < num:
+"""
+Sequência de Fibonacci é a sequência numérica proposta pelo matemático Leonardo Pisa
+Entendendo o fibonacci:
+Serie de fibonacci comeca com 0 e 1
+A = 0
+b = 1
+a  b
+0  1
+0 + 1 = 1
+1 + 1 = 2
+1 + 2 = 3
+Assim, começando pelo 1, essa sequência é formada somando cada numeral com o numeral que o antecede
+No caso do 1, repete-se esse numeral e soma-se, ou seja, 1 + 1 = 2.
+"""
+def fibonacci(tam):
+    a , b = 0 , 1
+    while a < tam:
 	a , b = b , b + a
 	print(a, end = " ")
-print()
+    print()
